90976b1e748a79ff74dfe87dc69e3c540450396f
Melissa D'Agruma <mdagruma@gmail.com>
2015-04-08 14:34:46 -0400

    added new contact page and forms for it

--- crm.rb
+++ crm.rb
@@ -1,17 +1,18 @@
 require_relative 'contact'
+require_relative 'rolodex'
 require 'sinatra'
+
+$rolodex = Rolodex.new
 
 get '/' do
 	@crm_app_name = "My CRM"
 	erb :index
 end
 
-get "/contacts" do
-  @crm_app_name = "My CRM"
-  @contacts = []
-  @contacts << Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer")
-  @contacts << Contact.new("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
-  @contacts << Contact.new("Sergey", "Brin", "sergey@google.com", "Co-Founder")
-
+get '/contacts' do
   erb :contacts
+end
+
+get '/contacts/new' do
+	erb :new_contact
 end
