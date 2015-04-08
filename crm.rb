4b22686e513b6c5de9b676f747190960b578f59c
Melissa D'Agruma <mdagruma@gmail.com>
2015-04-08 14:06:57 -0400

    added 'fake' contact info to display on the contacts page

--- crm.rb
+++ crm.rb
@@ -1,3 +1,4 @@
+require_relative 'contact'
 require 'sinatra'
 
 get '/' do
@@ -5,7 +6,12 @@ get '/' do
 	erb :index
 end
 
-get '/:contacts' do
-	@crm_app_name = "My CRM"
-	erb :contacts
+get "/contacts" do
+  @crm_app_name = "My CRM"
+  @contacts = []
+  @contacts << Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer")
+  @contacts << Contact.new("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
+  @contacts << Contact.new("Sergey", "Brin", "sergey@google.com", "Co-Founder")
+
+  erb :contacts
 end
