3782b3310d8897f2d3d0512afc04c1339b9cf46d
Melissa D'Agruma <mdagruma@gmail.com>
2015-04-09 14:10:00 -0400

    added header, footer and nav tags and style the fonts

--- crm.rb
+++ crm.rb
@@ -5,17 +5,14 @@ require 'sinatra'
 $rolodex = Rolodex.new
 
 get '/' do
-	@crm_app_name = "My CRM"
 	erb :index
 end
 
 get '/contacts' do
-	@crm_app_name = "My CRM"
   	erb :contacts
 end
 
 get '/contacts/new' do
-	@crm_app_name = "My CRM"
 	erb :new_contact
 end
 
